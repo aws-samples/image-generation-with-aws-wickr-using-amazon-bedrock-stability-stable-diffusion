6481207cf8ae88edde970f2506645b8beb48788a
Charles Chowdhury-Hanscombe <charlcch@amazon.com>
2023-11-21 13:58:06 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,17 +1,63 @@
-## My Project
+# Image Generation with AWS Wickr using Amazon Bedrock and the Stable Diffusion XL model
 
-TODO: Fill this README out!
+This sample will allow you to deploy a Generative AI text-to-image bot to your AWS Wickr network. It uses Amazon Bedrock and the Stable Diffusion XL model. 
 
-Be sure to:
+## Prerequisites and limitations
 
-* Change the title in this README
-* Edit your repository description on GitHub
+## Prerequisites
 
-## Security
+- Access to Amazon Bedrock, and the **stability.stable-diffusion-xl-v0** model enabled 
+- An existing AWS Wickr bot username and password
+- A supported host with Docker CE installed. This repo was tested on Ubuntu 22.04
+- IAM credentials configured on the host in the `~/.aws/config` and `~/.aws/credentials` file (see [here](https://docs.aws.amazon.com/cli/latest/reference/configure/) for details). The user must have the following IAM policy at a minimum (update with your Bedrock region):
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "bedrock:InvokeModel"
+            ],
+            "Resource": [
+                "arn:aws:bedrock:<region>::foundation-model/stability.stable-diffusion-xl-v0"
+            ]
+        }
+    ]
+}
+```
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+## Limitations
+
+This bot will store sent voice-memos on the host where the bot is running, inside the `/opt/WickrIO/clients/<username>/attachments/` directory. To periodically delete these, you could configure a crontab with the following entry to delete data when it is older than 1 day: `0 0 * * * /usr/bin/find /opt/WickrIO/clients/stable-diffusion_bot/attachments/ -name "*" -type f -mtime +1 -exec rm -f {} \;`
+
+## Installation
+
+1. Clone this repo and then enter the directory.
+2. Confirm you are within the repo directory, then zip and compress the contents: `tar czvf software.tar.gz *`. This will leave you with a file called `software.tar.gz`
+3. Create the bot folder: `sudo mkdir /opt/WickrIO`
+4. Copy this file to /opt/WickrIO: `sudo cp software.tar.gz /opt/WickrIO/`
+5. Start the WickrIO container with the following command: `docker run -v /home/ubuntu/.aws:/home/wickriouser/.aws -v /opt/WickrIO:/opt/WickrIO --restart=always --name="stable-diffusion_bot" -ti public.ecr.aws/x3s2s6k3/wickrio/bot-cloud:latest`
+6. You will now be at the command line within the bot.
+
+## Configuration
+
+1. Select `yes` if you wish see welcome message on startup
+2. Enter `add` when prompted to enter a command
+3. Enter the bot username and password when prompted
+4. Select `yes` to auto-login
+5. When asked to **Enter the bot configuration to use**, enter `import`
+6. When asked for the location, enter `/opt/WickrIO`
+7. Enter `stable-diffusion_bot` for the integration name
+
+The bot will now install.
+
+8. Enter your Amazon Bedrock region and credential profile when asked
+9. When you see **Successfully added record to the database!**, enter `start` and then provide the password when prompted
+10. Type `list` to see the status of the bot, it should say **Running** after a few seconds
+11. Send a message to the bot!
 
 ## License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
-
---
+++ bot-code.js
@@ -0,0 +1,155 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+const WickrIOAPI = require('wickrio_addon');
+const WickrIOBotAPI = require('wickrio-bot-api');
+const util = require('util')
+const logger = require('wickrio-bot-api').logger
+const { fromIni } = require('@aws-sdk/credential-providers');
+const { BedrockRuntimeClient, InvokeModelCommand } = require('@aws-sdk/client-bedrock-runtime')
+const { v4: uuidv4 } = require('uuid');
+
+console.log = function () {
+  logger.info(util.format.apply(null, arguments))
+}
+console.error = function () {
+  logger.error(util.format.apply(null, arguments))
+}
+
+var fs = require('fs');
+
+module.exports = WickrIOAPI;
+process.stdin.resume(); // so the program will not close instantly
+var bot;
+
+
+async function exitHandler(options, err) {
+  try {
+    var closed = await bot.close();
+    console.log(closed);
+    if (err) {
+      console.log("Exit Error:", err);
+      process.exit();
+    }
+    if (options.exit) {
+      process.exit();
+    } else if (options.pid) {
+      process.kill(process.pid);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//catches ctrl+c and stop.sh events
+process.on('SIGINT', exitHandler.bind(null, {
+  exit: true
+}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {
+  pid: true
+}));
+process.on('SIGUSR2', exitHandler.bind(null, {
+  pid: true
+}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {
+  exit: true
+}));
+
+/////////////////////////////////////
+
+// Respond to user's input.
+async function createResponse(message, vGroupID) {
+  // setup the clients
+  bot.processesJsonToProcessEnv()
+  var tokens = JSON.parse(process.env.tokens)
+  const REGION = tokens.AWS_REGION.value
+  const profileName = tokens.AWS_PROFILENAME.value
+
+  console.log('entered createResponse')
+
+  const client = new BedrockRuntimeClient({
+    region: REGION,
+    credentials: fromIni({profile: profileName,}),
+  });
+
+  const body = JSON.stringify({
+    text_prompts: [
+      {
+        text: message
+      }
+    ],
+    cfg_scale: 20,
+    steps: 30
+  });
+  
+  const modelId = 'stability.stable-diffusion-xl-v0';
+  
+  const invokeOptions = {
+    body,
+    modelId,
+    accept: '*/*',
+    contentType: 'application/json'
+  };
+
+  const command = new InvokeModelCommand(invokeOptions);
+  const response = await client.send(command);
+  const decoded = JSON.parse(
+    new TextDecoder().decode(response.body)
+  );
+  console.log('response');
+  const filename = uuidv4() + '.jpeg';
+  const filePath = `../../attachments/${filename}`
+  console.log('filename is: ', filename)
+  let buff = new Buffer.from(decoded.artifacts[0].base64, 'base64'); // added Buffer.from
+  fs.writeFileSync(`../../attachments/${filename}`, buff);
+  console.log('file written');
+  console.log('file path is ', filePath)
+  WickrIOAPI.cmdSendRoomAttachment(vGroupID, filename)
+}
+
+async function main() { // entry point
+  logger.info('entering main')
+  try {
+    var status;
+    if (process.argv[2] === undefined) {
+      var bot_username = fs.readFileSync('client_bot_username.txt', 'utf-8');
+      bot_username = bot_username.trim();
+      bot = new WickrIOBotAPI.WickrIOBot();
+      status = await bot.start(bot_username)
+    } else {
+      bot = new WickrIOBotAPI.WickrIOBot();
+      status = await bot.start(process.argv[2])
+    }
+    if (!status) {
+      exitHandler(null, {
+        exit: true,
+        reason: 'Client not able to start'
+      });
+    }
+
+    await bot.startListening(listen); // passes a callback function that will receive incoming messages into the bot client
+  } catch (err) {
+    logger.error(err);
+  }
+}
+
+async function listen(rMessage) { // starts a listener. Message payload accessible as 'message' 
+  logger.info('entering listen')
+  rMessage = JSON.parse(rMessage);
+  var sender = rMessage.sender;
+  var vGroupID = rMessage.vgroupid;
+  var userArr = [];
+  userArr.push(sender);
+  if (rMessage.message) {
+    var request = rMessage.message;
+
+    console.log('calling createResponse()')
+    await createResponse(request, vGroupID)
+  }
+}
+
+main();
